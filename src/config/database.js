44f2fcbfb2a673cd45535eea13036b4964f19771
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-05-01 19:22:20 -0300

    Initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
+const databaseName = require('../credentials/database.json').database
+const databaseUser = require('../credentials/database.json').user
+const databasePassword = require('../credentials/database.json').password
+
 module.exports = {
   dialect: 'postgres',
   host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
-  database: 'gonodemodulo2',
+  username: databaseUser,
+  password: databasePassword,
+  database: databaseName,
   operatorAliases: false,
   define: {
     timestamps: true,
